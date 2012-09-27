60a8574ca4b4ef9cbfb6aad9e8688a3694a04c60
keren <ke.lane.ren@gmail.com>
2012-09-27 15:35:06 +0100

    added more test

--- scalajson/src/test/scala/com/spaceape/open/common/json/JSONTest.scala
+++ scalajson/src/test/scala/com/spaceape/open/common/json/JSONTest.scala
@@ -10,42 +10,49 @@ import org.junit._
 import Assert._
 
 class JSONTest {
-	@Test def test_add_field(){
-		var str = "{\"name\": \"PP\"}"
-		var jsonO = Json.parse(str).as[JsObject]
-		jsonO = jsonO.++(JsObject(List("time" -> JsNumber(190))))
-		
-		var expecting = """{"name":"PP","time":190}"""
-		assertEquals(expecting, Json.stringify(jsonO))
-	}
-	
-	@Test def test_query_optional_field(){
-		var str = "{\"name\": \"PP\"}"
-		var jsonO = Json.parse(str)
-		var result = jsonO \ "time"
-		assertTrue(result.isInstanceOf[JsUndefined])
-		
-		result = jsonO \ "name"
-		assertTrue(result.isInstanceOf[JsString])
-	}
-	
-	@Test def test_deserialize(){
-		var str = """ {"token":"2","groupname":"default","message":{"username":"Lu","@type":"UserSignIn"},"@type":"BroadcastRequest"} """
-	    var jsonO = Json.parse(str)
-	    
-	    assertEquals(JsString("default"), jsonO \ "groupname")
-	    
-		var result = jsonO \ "message"
-		
-		assertEquals(JsString("Lu"), result \ "username")
-	}
-	
-	@Test def test_set_value(){
-	    var str = """ {"token":"2","groupname":"default"} """
-	    var jsonO = Json.parse(str).asInstanceOf[JsObject]
-	    
-	    jsonO = jsonO.++ (JsObject(List("groupname" -> JsString("newname"))))
-	    
-	    assertEquals(JsString("newname"), jsonO \ "groupname")
-	}
+    @Test def test_add_field() {
+        var str = "{\"name\": \"PP\"}"
+        var jsonO = Json.parse(str).as[JsObject]
+        jsonO = jsonO.++(JsObject(List("time" -> JsNumber(190))))
+
+        var expecting = """{"name":"PP","time":190}"""
+        assertEquals(expecting, Json.stringify(jsonO))
+    }
+
+    @Test def test_query_optional_field() {
+        var str = "{\"name\": \"PP\"}"
+        var jsonO = Json.parse(str)
+        var result = jsonO \ "time"
+        assertTrue(result.isInstanceOf[JsUndefined])
+
+        result = jsonO \ "name"
+        assertTrue(result.isInstanceOf[JsString])
+    }
+
+    @Test def test_deserialize() {
+        var str = """ {"token":"2","groupname":"default","message":{"username":"Lu","@type":"UserSignIn"},"@type":"BroadcastRequest"} """
+        var jsonO = Json.parse(str)
+
+        assertEquals(JsString("default"), jsonO \ "groupname")
+
+        var result = jsonO \ "message"
+
+        assertEquals(JsString("Lu"), result \ "username")
+    }
+
+    @Test def test_set_value() {
+        var str = """ {"token":"2","groupname":"default"} """
+        var jsonO = Json.parse(str).asInstanceOf[JsObject]
+
+        jsonO = jsonO.++(JsObject(List("groupname" -> JsString("newname"))))
+
+        assertEquals(JsString("newname"), jsonO \ "groupname")
+    }
+
+    @Test def test_query_long_when_field_not_exist() {
+        var str = """ {"token":"2","groupname":"default"} """
+        var jsonO = Json.parse(str).asInstanceOf[JsObject]
+        var rs = (jsonO \ "timestamp").as[Option[Long]]
+        assertEquals(None, rs)
+    }
 }
